096d99190970ad14fb14ab9cdf00d93b25cd1ae3
Bharani <bharanidharan.kaliyaperumal@tesco.com>
2022-03-05 22:41:07 +0530

    fixing img path

--- cuisine_predictor.py
+++ cuisine_predictor.py
@@ -17,7 +17,7 @@ def predict_cuisine(ingredients):
 st.subheader('Ingredients')
 # st.write('Enter the ingredients you have')
 
-def file_selector(folder_path='.'):
+def file_selector(folder_path='./img'):
     filenames = os.listdir(folder_path)
     selected_filename = st.selectbox('Select a file', filenames)
     return os.path.join(folder_path, selected_filename)
